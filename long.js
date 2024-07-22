3031354a4e303c01c25515e61ee52320f72186d9
ABDULLAHI ABDIRIZAK MOHAMED <105462733+Abdallahdari@users.noreply.github.com>
2024-07-22 22:53:21 +0300

    finish

--- long.js
+++ long.js
@@ -1,21 +1,21 @@
 const namee = document.getElementById("name-login");
-const password = document.getElementById('Password-login');
-const btnlogin = document.getElementById('login-btn');
+const password = document.getElementById("Password-login");
+const btnlogin = document.getElementById("login-btn");
 
 // console.log(loginn, namee, password, btnlogin);
 
 const data1 = {
-  Email: 'abdallah',
-  pin: '123'
+  Email: "abdallah",
+  pin: "4423",
 };
 
-btnlogin.addEventListener('click', function(e) {
+btnlogin.addEventListener("click", function (e) {
   e.preventDefault();
-  if (namee.value === '' || password.value === '') {
-    console.log('no email or password entered');
+  if (namee.value === "" || password.value === "") {
+    console.log("no email or password entered");
   } else if (namee.value === data1.Email && password.value === data1.pin) {
-window.location.href='user1.html'
-} else {
-    console.log('wrong username or password');
+    window.location.href = "user1.html";
+  } else {
+    console.log("wrong username or password");
   }
 });
